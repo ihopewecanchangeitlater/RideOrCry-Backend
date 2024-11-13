ec6a70934a9ea9d522fe88e391aa9b48eeb9b279
Kostas Thomson <kostasthomson12@gmail.com>
2024-11-13 20:04:31 +0200

    initial

---
+++ src/main/java/gr/uom/RideOrCry/RideOrCryApplication.java
@@ -0,0 +1,13 @@
+package gr.uom.RideOrCry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideOrCryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideOrCryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/uom/RideOrCry/RideOrCryApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.uom.RideOrCry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideOrCryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
